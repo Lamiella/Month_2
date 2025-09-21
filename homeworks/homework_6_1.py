ddfcf0e6a4821e493ba7a4de8a24467bcfc2fa28
Alena Kucherenko <alenka110198@gmail.com>
2025-09-21 11:56:11 +0600

    add homework_6_1

--- homeworks/homework_6_1.py
+++ homeworks/homework_6_1.py
@@ -1,19 +1,21 @@
 from datetime import datetime as dt
+from time import sleep
 
-
-
-time_now = dt.now()
-print(time_now.year)
-print(time_now.month)
-print(time_now.day)
-print(time_now.hour)
-print(time_now.minute)
-print(time_now.second)
-
+def checktime(func):
+    def wrapper(*args, **kwargs):
+        start_time = dt.now()
+        print(f'Функция была вызвана в {start_time.hour}:{start_time.minute}:{start_time.second} '
+              f'{start_time.day}/{start_time.month}/{start_time.year}')
+        result = func(*args, **kwargs)
+        end_time = dt.now()
+        print(f'Функция была закончена в {end_time.hour}:{end_time.minute}:{end_time.second} '
+              f'{end_time.day}/{end_time.month}/{end_time.year}')
+        return result
+    return wrapper
 
 @checktime
 def hello_world():
     print("hello world")
-
+    sleep(1)
 
 hello_world()
